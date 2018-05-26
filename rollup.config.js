e87b68732a7d9f0d1ea44e87f3c94a4741fafa67
David Lee <david.isaac.lee@gmail.com>
2018-05-26 11:12:49 -0700

    Add Node module resolution Rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from 'typescript';
 import typescriptPlugin from 'rollup-plugin-typescript';
 import glsl from 'rollup-plugin-glsl';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
 	input: 'src/runner.ts',
@@ -14,6 +15,7 @@ export default {
 		typescriptPlugin({
 			typescript
 		}),
+		nodeResolve(),
   ]
 };
 
