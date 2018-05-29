b18c7353b6d7473997aac80703d9b7772be8db99
David Lee <david.isaac.lee@gmail.com>
2018-05-29 13:10:01 -0700

    Add scaling argument to resizeCanvas

--- src/utility/resizeCanvas.ts
+++ src/utility/resizeCanvas.ts
@@ -1,7 +1,8 @@
 // https://webglfundamentals.org/webgl/lessons/webgl-resizing-the-canvas.html
-export function resizeCanvas(canvas: HTMLCanvasElement) {
-	var realToCSSPixels = window.devicePixelRatio;
-
+export function resizeCanvas(
+	canvas: HTMLCanvasElement,
+	realToCSSPixels: number = window.devicePixelRatio,
+) {
 	// Lookup the size the browser is displaying the canvas in CSS pixels
 	// and compute a size needed to make our drawingbuffer match it in
 	// device pixels.
