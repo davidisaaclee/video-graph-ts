bfa4325407e8480ceb9a46036000464ec2520f91
David Lee <david.isaac.lee@gmail.com>
2018-05-29 19:44:27 -0700

    Remove unused code

--- src/utility/Graph.ts
+++ src/utility/Graph.ts
@@ -4,7 +4,10 @@ export interface Graph<Node, EdgeMetadata> {
 	edges: { [key: string]: { src: string, dst: string, metadata: EdgeMetadata } };
 }
 
-export type ExecutionStep = { nodeKey: string, edges: Array<{ edgeKey: string, beginsCycle: boolean }> }
+export type ExecutionStep = {
+	nodeKey: string,
+	edges: Array<{ edgeKey: string, beginsCycle: boolean }>
+};
 
 export function resolveDependencies<Node>(
 	graph: Graph<Node, any>,
@@ -42,64 +45,6 @@ export function resolveDependencies<Node>(
 }
 
 export const executionOrder = resolveDependencies;
-
-/*
-// TODO: Like crazy optimize this lol
-export function executionOrder<Node>(graph: Graph<T>, outputNodeKey: string): Array<ExecutionStep> {
-	// Nodes ordered by execution order.
-	// We'll work through this in the second phase.
-	const sortedNodes = [outputNodeKey];
-
-	const visited = new Set(sortedNodes);
-	const queue = [outputNodeKey];
-
-	for (let currentNode = queue.shift(); currentNode != null; currentNode = queue.shift()) {
-		const srcNodes =
-			new Set(
-				Array.from(edgesWithDestination(currentNode, graph))
-					.map(edgeKey => graph.edges[edgeKey].src));
-
-		// Without cycles
-		const newSrcNodes = 
-			setDifference(
-				srcNodes,
-				visited);
-		newSrcNodes.forEach(x => {
-			visited.add(x);
-			sortedNodes.unshift(x);
-			queue.push(x);
-		});
-	}
-
-	// Reuse visited set variable.
-	visited.clear();
-	const result = [];
-	for (const nodeKey of sortedNodes) {
-		// Add the current node to visited.
-		// If the current node has an edge to itself, it should be considered a cycle.
-		visited.add(nodeKey);
-
-		const edges = Array.from(edgesWithSource(nodeKey, graph))
-			.map(edgeKey => {
-				const dst = graph.edges[edgeKey].dst;
-
-				return {
-					edgeKey,
-					// This edge begins a cycle if it is pointing to a node that
-					// we've already visited.
-					beginsCycle: visited.has(dst)
-				}
-			});
-
-		result.push({
-			nodeKey,
-			edges
-		});
-	}
-
-	return result;
-}
-*/
 
 export function edgesWithSource<Node, EdgeMetadata>(srcKey: string, graph: Graph<Node, EdgeMetadata>): Set<string> {
 	const result = new Set();
